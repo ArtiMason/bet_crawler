9d2c050499104906bdf77f7512ed7f3977f9a047
ArtiMason <mister.gula@bk.ru>
2026-09-24 17:06:13 +0300

    Modified ranker

--- core/ranker.py
+++ core/ranker.py
@@ -4,13 +4,14 @@ class LinkRanker:
     def __init__(self):
         self.keywords = ["прогноз", "ставка", "матч", "bet", "tip", "analyze", "prediction"]
 
-    def rank_link(self, links: List[str]) -> List[str]:
+    def rank_link(self, links: List[Tuple[str,str]]) -> List[str]:
         filtered_links = []
-        for link in links:
-            link_lower = link.lower()
-            if any(word in link_lower for word in self.keywords):
-                filtered_links.append(link)
-        return filtered_links
+        for url, title in links:
+            score = self.score_link(url, title)
+            if score > 0:
+                filtered_links.append((score, url))
+        filtered_links.sort(key=lambda x: x[0], reverse=True)        
+        return [url for score, url in filtered_links]
     def score_link(self, url: str, title: str = "") -> int:
         score = 0 
         text_to_analyze = (url + " " + title).lower()
