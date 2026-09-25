70e28cf5f011c879f186e36fa50d3b384fe5c996
ArtiMason <mister.gula@bk.ru>
2026-09-25 16:26:42 +0300

    Implement browser module: added playwright

--- fetcher/browser.py
+++ fetcher/browser.py
@@ -1,25 +1,43 @@
 from playwright.async_api import async_playwright
 from typing import Optional
+import asyncio
 
 class BrowserHandler:
     def __init__(self, timeout: int = 30000):
         self.timeout = timeout
+        self.browser = None
+        self.context = None
+        self.playwright = None
 
-    async def get_title(self, url:str) -> Optional[str]:
-        async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=True)
-            context = await browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-            page = await context.new_page()
+    async def start(self):
+        if not self.browser:
+            self.playwright = await async_playwright().start()
+            self.browser = await self.playwright.chromium.launch(headless=True)
+            self.context = await self.browser.new_context(user_agent="Mozilla/5.0 "
+            "(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+            viewport={'width': 1920, 'height': 1080}) 
 
-            try:
-                await page.goto(url, timeout=self.timeout, wait_until="domcontentloaded")
-                title = await page.title()
-                return title
-            except Exception as e:
-                print(f"Browser error fot {url} : {e}")
-                return None
-            finally:
-                await browser.close()
+    async def get_title(self, url: str) -> Optional[str]:
+        if not self.context:
+            await self.start()
+
+        page: Page = await self.context.new_page()
+        try:
+            await page.goto(url, timeout=self.timeout, wait_until="domcontentloaded")
+            title = await page.title()
+            return title if title else None
+        except Exception as e:
+            print(f"Playwright error for {url} : {e}")
+            return None
+        finally:
+            await page.close()
+
+    async def stop(self):
+        if self.browser:
+            await self.browser.close()
+        if self.playwright:
+            await self.playwright.stop()            
+
 
 
        
--- test_search.py
+++ test_search.py
@@ -1,31 +1,34 @@
 import asyncio
 from search.duckduckgo import DuckDuckGoSearch
-from fetcher.request import RequestHandler
+from fetcher.browser import BrowserHandler
 from core.ranker import LinkRanker
 
 async def main():
     query = "Арсенал - Лидс прогноз"
 
     search_engine = DuckDuckGoSearch()
-    fetcher = RequestHandler()
+    browser = BrowserHandler()
     ranker = LinkRanker()
 
-    print("Searching...")
-    ddg_links = await search_engine.search(query)
-    print(f"Found {len(ddg_links)} links")
+    try:
+        ddg_links = await search_engine.search(query)
 
-    print("Fetching titles for accuracy...")
-    links = []
-    for url in ddg_links[:15]:
-        title = await fetcher.get_title(url)
-        if title:
-            links.append((url,title))
-            print(f"OK, {title[:50]}..")
+        await browser.start()
+        print("Fetching titles..")
+        links = []
+        for url in ddg_links[:10]:
+            title = await browser.get_title(url)
+            if title:
+                links.append((url,title))
+                print(f"OK: {title[:50]}...") 
 
-    final_links = ranker.rank_link(links)
-    print(f"\nFinal filtered links: {len(final_links)}")
-    for i, link in enumerate(final_links[:5],1):
-        print(f"{i}. {link}")        
+        final_links = ranker.rank_link(links)
+        print(f"\nFinal filtered links: {len(final_links)}")
+        for i, link in enumerate(final_links,1):
+            print(f"{i}. {link}")
+
+    finally:
+        await browser.stop()                
 
 if __name__ == "__main__":
     asyncio.run(main())    
